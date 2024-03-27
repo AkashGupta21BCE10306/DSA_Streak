c60e82ed39bcf99c2345ddd6d76b8a835da1cb6d
AkashGupta21BCE10306 <144457581+AkashGupta21BCE10306@users.noreply.github.com>
2024-03-27 15:15:57 +0530

    day21

---
+++ day21.cpp
@@ -0,0 +1,50 @@
+//Find shortest safe route in a matrix
+#include<iostream>
+using namespace std;
+class Solution
+{
+public:
+    int findShortestPath(vector<vector<int>> &mat)
+    {
+    //   lets make the adjacent cells of landmines unsafe by marking them 0
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                if(mat[i][j]==0){
+                    if(i-1 >= 0)mat[i-1][j]=-1;
+                    if(i+1< mat.size())mat[i+1][j]=-1;
+                    if(j-1>=0)mat[i][j-1]=-1;
+                    if(j+1<mat[0].size())mat[i][j+1]=-1;
+                }
+            }
+        }
+        int n=mat.size(),m=mat[0].size();
+        // now we can move normally from left columns 1 to right columns
+        queue<pair<int,pair<int,int>>>q;
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+        for(int i=0;i<n;i++){
+            if(mat[i][0]==1)q.push({1,{i,0}});
+        }
+        //  to move in 3 direction ( no need to go backward )
+        int dx[4] = {-1, 0, 1};
+        int dy[4] = {0, 1, 0,};
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            int steps=it.first;
+            int row=it.second.first;
+            int col=it.second.second;
+            if(col==m-1)return steps;
+            // from current cell we can move to 4 dir (no need to move backward/right)
+            for(int i=0;i<3;i++){
+                int newR=row+dx[i];
+                int newC=col+dy[i];
+                if(newR>=0 && newR<n && newC>=0 && newC<m && mat[newR][newC]==1 && dist[newR][newC] > steps+1){
+                    q.push({steps+1,{newR,newC}});
+                    dist[newR][newC]=steps+1;
+                }
+            }
+        }
+        return -1;
+       
+    }
+};
