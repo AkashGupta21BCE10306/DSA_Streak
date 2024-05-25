8c5ad6d17dc781cb370bc838a8e5c09df22fe8e5
AkashGupta21BCE10306 <144457581+AkashGupta21BCE10306@users.noreply.github.com>
2024-05-25 12:26:54 +0530

    day71

---
+++ day71.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    /*You are requried to complete this method */
+    long long max_Books(int nums[], int n, int k) {
+        long long sum = 0, ans = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] <= k) {
+                sum += nums[i];
+                ans = max(ans, sum);
+            }
+            if(nums[i] > k) {
+                sum = 0;
+            }
+            
+        }
+        return ans;
+    }
+}; 
+
